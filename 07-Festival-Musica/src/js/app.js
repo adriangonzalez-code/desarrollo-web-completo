e01ab3f755480eebbb33d56440cc80850a5d71c4
Adrián González <g.adrian.gonzalez.90@gmail.com>
2024-10-23 21:14:43 -0600

    Completed Fest project

--- 07-Festival-Musica/src/js/app.js
+++ 07-Festival-Musica/src/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     navegacionFija();
     crearGaleria();
     resaltarEnlace();
+    scrollNav();
 });
 
 function navegacionFija() {
@@ -79,8 +80,37 @@ function resaltarEnlace() {
         const sections = document.querySelectorAll('section');
         const navLinks = document.querySelectorAll('.navegacion-principal a');
 
+        let actual = '';
         sections.forEach(section => {
-            const sectionTop = section;
-        })
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (window.scrollY >= (sectionTop -sectionHeight / 3)) {
+                actual = section.id;
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active');
+            }
+        });
+    });
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+    navLinks.forEach(link=> {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 }
